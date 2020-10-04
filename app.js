2b7772fd7f51ef3b25a3cfa1580f4cc3ba382ad2
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-10-04 10:23:19 +0100

    Second stage, rearrange characters in word alphabetically and compare, if a match word is anagram

--- app.js
+++ app.js
@@ -6,8 +6,23 @@ function compareWords(firstWord, secondWord){
     console.log(first);
     console.log(second);
 
+    //If words are not same length, or if they are the exact same word, they are not anagrams
+    if (first.length != second.length || first === second) {
+        console.log(`${firstWord} and ${secondWord} are not anagrams.`);
+        return false;
+    } else {
+        //Split word into component characters, alphabetise, then re-stringify
+        //If sorted words are the same, they are anagrams
+        first = first.split("").sort().join("");
+        second = second.split("").sort().join("");
+            if (first === second) {
+                console.log(`${firstWord} and ${secondWord} are anagrams.`);
+                return true;
+            }
+    } 
 }
 
 
 
 compareWords("squirrel", "Squi rrel@");
+compareWords("Fantastic", "TicTansFa");
